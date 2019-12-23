fc69f00d692c24de13f0844039913667f822b2a7
Марина Ивановић <im160150d@student.etf.bg.ac.rs>
2019-12-23 19:39:38 +0100

    Time calculation changed

--- clock-time-detection/extractTime.m
+++ clock-time-detection/extractTime.m
@@ -6,7 +6,7 @@ function [hour, min, sec] = extractTime(I)
     
     % Calculate time from the needle lines
     hour = getTime(I, hourNeedle, 12);
-    min = getTime(I, minNeedle, 60);
+    min = getTime(I, minNeedle, 60); % floor
     if ~exist
         sec = -1;
     else
--- clock-time-detection/needlesUtils/getTime.m
+++ clock-time-detection/needlesUtils/getTime.m
@@ -2,7 +2,7 @@ function time = getTime(I, needle, unitsPerCircle)
     middlePoint = getMiddlePoint(I);
     
     shiftedNeedleCoords = shiftNeedleCoords(needle, middlePoint);
-    %plotShiftedNeedle(I, shiftedNeedleCoords, middlePoint); input('X:');
+    % plotShiftedNeedle(I, shiftedNeedleCoords, middlePoint);
         
     time = getAngle(shiftedNeedleCoords) / (2*pi) * unitsPerCircle;
 end
@@ -21,22 +21,40 @@ function shifted = shiftNeedleCoords(needle, middlePoint)
     p2(1) = p2(1) - middlePoint(1);
     p2(2) = -p2(2) + middlePoint(2);
     
-    shifted.point1 = p1;
-    shifted.point2 = p2;
+    shifted = orderPoints(p1, p2, middlePoint);
+end
+
+function shifted = orderPoints(p1, p2, middlePoint)
+    mqs1 = sqrt(p1(1)^2 + p1(2)^2);
+    mqs2 = sqrt(p2(1)^2 + p2(2)^2);
+
+    if mqs1 < mqs2
+        shifted.point1 = p1;
+        shifted.point2 = p2;
+    else
+        shifted.point1 = p2;
+        shifted.point2 = p1;
+    end
 end
 
 function angle = getAngle(needle)
-    angle = atan2(needle.point1(1) - needle.point2(1), ...
-                  needle.point1(2) - needle.point2(2));
+    angle = atan2(needle.point2(1) - needle.point1(1), ...
+                  needle.point2(2) - needle.point1(2));
+    if angle < 0
+        angle = pi + abs(angle);
+    end
 end
 
 function plotShiftedNeedle(I, needle, middlePoint)
-    figure(); hold on;
+    figure; hold on;
 
     xy = [needle.point1; needle.point2];
     plot(xy(:,1),xy(:,2), 'LineWidth', 2, 'Color', 'green');
+    plot(needle.point1(1), needle.point1(2), '*', 'Color', 'red');
+    plot(needle.point2(1), needle.point2(2), '*', 'Color', 'blue');
     
     xlim([-middlePoint(1), middlePoint(1)]);
     ylim([-middlePoint(2), middlePoint(2)]);
+    legend('Whole line', 'Closer to middle', 'Further from middle');
     hold off;
 end
